1d53b0b6c35f2f93eeec9433437a89e1167fbb10
Maxime22 <maxime.sabouret@evaneos.com>
2024-02-23 11:44:28 +0100

    update implementation to get result of multiplication and make test work

--- src/Dollar.php
+++ src/Dollar.php
@@ -12,8 +12,8 @@ class Dollar
         $this->amount = $amount;
     }
 
-    public function times(int $multiplier)
+    public function times(int $multiplier) : Dollar
     {
-        $this->amount = $this->amount * $multiplier;
+        return new Dollar($this->amount * $multiplier);
     }
 }
