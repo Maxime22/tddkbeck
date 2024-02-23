d6082e6a11fad20d2a31bad6b4178faefde8adca
Maxime22 <maxime.sabouret@evaneos.com>
2024-02-23 11:34:22 +0100

    add other call to times but impossible to get the result for now

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -19,5 +19,7 @@ class MoneyTest extends TestCase
         $five = new Dollar(5);
         $five->times(2);
         $this->assertEquals(10, $five->amount);
+        $five->times(3);
+        $this->assertEquals(15, $five->amount);
     }
 }
