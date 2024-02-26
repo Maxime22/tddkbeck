6a09ff6fa703b7ebe72e6b1e63c07d898ff0f948
Maxime22 <maxime.sabouret@evaneos.com>
2024-02-26 17:40:56 +0100

    chapter 4 done

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 // $5 + 10CHF = $10 if rate is 2:1
 // $5 * 2 = $10 ✔️
-// Make "amount" private
+// Make "amount" private ✔️
 // Dollar side-effects ? ✔️
 // Money rounding ?
 // equals() ✔️
