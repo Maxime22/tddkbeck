3b39fa5df00a47411859959866dd4f42f4132e4f
Maxime22 <maxime.sabouret@evaneos.com>
2024-02-26 17:53:41 +0100

    chapter 5 done

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -14,7 +14,10 @@ use PHPUnit\Framework\TestCase;
 // hashCode()
 // Equal null
 // Equal object
-// 5 CHF * 2 = 10 CHF
+// 5 CHF * 2 = 10 CHF ✔️
+// Dollar/Franc duplication
+// Common equals
+// Common times
 
 class MoneyTest extends TestCase
 {
