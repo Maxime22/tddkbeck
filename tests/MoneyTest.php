da72e10c9baed1fa54b3c8aec0f63a11b37f6afe
Maxime22 <maxime.sabouret@evaneos.com>
2024-02-27 10:36:23 +0100

    tests: add factory method franc()

--- tests/MoneyTest.php
+++ tests/MoneyTest.php
@@ -1,7 +1,6 @@
 <?php
 declare(strict_types=1);
 
-use App\Franc;
 use App\Money;
 use PHPUnit\Framework\TestCase;
 
@@ -35,9 +34,9 @@ class MoneyTest extends TestCase
     /** @test **/
     public function francMultiplication(): void
     {
-        $five = new Franc(5);
-        $this->assertEquals(new Franc(10), $five->times(2));
-        $this->assertEquals(new Franc(15), $five->times(3));
+        $five = Money::franc(5);
+        $this->assertEquals(Money::franc(10), $five->times(2));
+        $this->assertEquals(Money::franc(15), $five->times(3));
     }
 
     /** @test */
@@ -45,8 +44,8 @@ class MoneyTest extends TestCase
     {
         $this->assertTrue((Money::dollar(5))->equals(Money::dollar(5)));
         $this->assertFalse((Money::dollar(5))->equals(Money::dollar(6)));
-        $this->assertTrue((new Franc(5))->equals(new Franc(5)));
-        $this->assertFalse((new Franc(5))->equals(new Franc(6)));
-        $this->assertFalse((new Franc(5))->equals(Money::dollar(5)));
+        $this->assertTrue((Money::franc(5))->equals(Money::franc(5)));
+        $this->assertFalse((Money::franc(5))->equals(Money::franc(6)));
+        $this->assertFalse((Money::franc(5))->equals(Money::dollar(5)));
     }
 }
