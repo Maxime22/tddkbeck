74a95e8ed84a44cc786bfdb56c771ff8feb22807
Maxime22 <maxime.sabouret@evaneos.com>
2024-02-28 18:30:34 +0100

    feat: make reduceSum test pass

--- src/Bank.php
+++ src/Bank.php
@@ -7,6 +7,9 @@ class Bank
 {
     public function reduce(ExpressionInterface $source, string $to): Money
     {
+        if($source instanceof Money){
+            return $source;
+        }
         $sum = $source;
         return $sum->reduce($to);
     }
